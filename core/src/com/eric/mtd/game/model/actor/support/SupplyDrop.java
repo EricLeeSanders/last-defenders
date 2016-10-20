86a703d5d27519f31aaef158e9d2a95a84e08241
EricLeeSanders <EricLeeSanders@gmail.com>
2016-10-19 21:17:42 -0400

    MTD - 52 - Added Supply Drop
    Added Supply Drop

---
+++ core/src/com/eric/mtd/game/model/actor/support/SupplyDrop.java
@@ -0,0 +1,68 @@
+package com.eric.mtd.game.model.actor.support;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.utils.Pool;
+import com.eric.mtd.game.model.actor.GameActor;
+import com.eric.mtd.game.service.factory.ActorFactory.SupplyDropPool;
+import com.eric.mtd.game.service.factory.interfaces.ISupplyDropFactory;
+import com.eric.mtd.util.ActorUtil;
+import com.eric.mtd.util.Dimension;
+import com.eric.mtd.util.Logger;
+import com.eric.mtd.util.Resources;
+
+public class SupplyDrop extends GameActor implements Pool.Poolable{
+	private static final float SUPPLYDROP_DURATION = 2f;
+	private boolean active;
+	private SupplyDropPool pool;
+	private ISupplyDropFactory supplyDropFactory;
+	public SupplyDrop(TextureRegion textureRegion, SupplyDropPool pool, ISupplyDropFactory supplyDropFactory) {
+		super(textureRegion, new Dimension(textureRegion.getRegionWidth(), textureRegion.getRegionHeight()));
+		this.pool = pool;
+		this.supplyDropFactory = supplyDropFactory;
+	}
+	
+	public void beginSupplyDrop(Vector2 dropLocation){
+		Logger.info("Beginning Supply drop");
+		active = true;
+		this.setRotation(-90);
+		setPositionCenter(new Vector2(0-this.getHeight(), dropLocation.y));
+		float moveToX = Resources.VIRTUAL_WIDTH+this.getHeight();
+		float moveToY = ActorUtil.calcYBotLeftFromCenter(dropLocation.y, getHeight());
+		this.addAction(Actions.moveTo(moveToX, moveToY,  SUPPLYDROP_DURATION, Interpolation.linear));
+		float dropDelay = SUPPLYDROP_DURATION * ((dropLocation.x - (this.getWidth() / 4))/ Resources.VIRTUAL_WIDTH);
+		Logger.info("DropDelay: " + dropDelay);
+		supplyDropFactory.loadSupplyDropCrate().beginDrop(dropDelay, dropLocation).toBack();
+	}
+	@Override
+	public void act(float delta) {
+		super.act(delta);
+		if(active){
+			if(this.getActions().size <= 0){
+				pool.free(this);
+			}
+		}
+	}
+
+	public void setActive(boolean active){
+		this.active = active;
+	}
+	
+	public boolean isActive(){
+		return active;
+	}
+	
+	@Override
+	public void reset() {
+		active = false;
+		this.setPosition(0, 0);
+		this.setRotation(0);
+		this.clear();
+		this.remove();
+	}
+}
---
+++ core/src/com/eric/mtd/game/model/actor/support/SupplyDropCrate.java
@@ -0,0 +1,142 @@
+package com.eric.mtd.game.model.actor.support;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.Shape2D;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.utils.Pool;
+import com.eric.mtd.game.helper.CollisionDetection;
+import com.eric.mtd.game.model.actor.GameActor;
+import com.eric.mtd.game.model.actor.combat.tower.Tower;
+import com.eric.mtd.game.service.factory.ActorFactory.SupplyDropCratePool;
+import com.eric.mtd.util.ActorUtil;
+import com.eric.mtd.util.Dimension;
+import com.eric.mtd.util.Logger;
+import com.eric.mtd.util.Resources;
+
+public class SupplyDropCrate extends GameActor implements Pool.Poolable{
+	private static final int COST = 1000;
+	private static final float SUPPLYDROP_DURATION = 1f;
+	private static final float RANGE = 300f;
+	private Circle rangeCircle = new Circle();
+	private boolean active, showRange;
+	private Sprite locationSprite;
+	private SupplyDropCratePool pool;
+	private Group towerGroup;
+	public SupplyDropCrate(TextureRegion textureRegion, SupplyDropCratePool pool, Group towerGroup) {
+		super(textureRegion, new Dimension(50, 50));
+		this.pool = pool;
+		this.towerGroup = towerGroup;
+		
+		Pixmap locationPixmap = new Pixmap(600, 600, Format.RGBA8888);
+		locationPixmap.setColor(0,0,0,0.5f);
+		locationPixmap.fillCircle(300,300,300);
+		locationSprite = (new Sprite(new Texture(locationPixmap)));
+		locationPixmap.dispose();
+		locationSprite.setSize(RANGE, RANGE);
+		float locX = ActorUtil.calcXBotLeftFromCenter(getX(), locationSprite.getWidth());
+		float locY = ActorUtil.calcYBotLeftFromCenter(getY(), locationSprite.getHeight());
+		locationSprite.setPosition(locX, locY);
+	}
+	
+	public SupplyDropCrate beginDrop(float dropDelay, Vector2 dropLocation){
+		Logger.info("Beginning Crate drop");
+		active = true;
+		setPositionCenter(dropLocation.x, dropLocation.y);
+		addAction(Actions.delay(dropDelay, Actions.scaleTo(0.5f, 0.5f, SUPPLYDROP_DURATION, Interpolation.linear)));
+		setVisible(false);
+		addAction(Actions.delay(dropDelay, Actions.visible(true)));
+		return this;
+	}
+	
+	@Override
+	public void act(float delta) {
+		super.act(delta);
+		if(active){
+			if(this.getActions().size <= 0){
+				healActors();
+				pool.free(this);
+			}
+		}
+	}
+	
+	
+	@Override
+	public void draw(Batch batch, float alpha) {
+		super.draw(batch, alpha);
+		if(isShowRange()){
+			locationSprite.draw(batch);
+		}
+	}
+	
+	private void healActors(){
+		Logger.info("Healing actors");
+		for(Actor actor : towerGroup.getChildren()){
+			if(actor instanceof Tower){
+				Tower tower = (Tower)actor;
+				if(CollisionDetection.targetWithinRange(tower.getBody(), getRangeShape())){
+					tower.heal();
+				}
+			}
+		}
+	}
+	
+	public void freeActor(){
+		pool.free(this);
+	}
+	
+	public void setShowRange(boolean showRange){
+		this.showRange = showRange;
+	}
+	
+	public boolean isShowRange(){
+		return showRange;
+	}
+	
+	public void setActive(boolean active){
+		this.active = active;
+	}
+	
+	public boolean isActive(){
+		return active;
+	}
+	
+	public int getCost(){
+		return COST;
+	}
+	
+	public Shape2D getRangeShape() {
+		rangeCircle.set(getPositionCenter().x, getPositionCenter().y, RANGE);
+		return rangeCircle;
+	}
+
+	@Override 
+	public void setPositionCenter(Vector2 pos){
+		super.setPositionCenter(pos);
+		locationSprite.setPosition(ActorUtil.calcXBotLeftFromCenter(pos.x, locationSprite.getWidth()), ActorUtil.calcYBotLeftFromCenter(pos.y, locationSprite.getHeight()));
+	}
+
+	@Override
+	public void reset() {
+		Logger.info("Resetting Supply Drop Crate");
+		active = false;
+		showRange = false;
+		this.setPosition(0, 0);
+		this.setRotation(0);
+		this.clear();
+		this.remove();
+		this.setScale(1);
+	}
+
+}
