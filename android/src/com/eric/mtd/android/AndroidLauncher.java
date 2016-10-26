03b465c7a0ce46609fbe5c8648a0156782207f71
Eric Sanders <EricLeeSanders@gmail.com>
2016-10-25 20:22:00 -0400

    MTD - 51 - Updated build
    Updated build and added dependencies for GPS

--- android/src/com/eric/mtd/android/AndroidLauncher.java
+++ android/src/com/eric/mtd/android/AndroidLauncher.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.eric.mtd.MTDGame;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 
 
 public class AndroidLauncher extends AndroidApplication {
@@ -11,6 +13,11 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.addApi(Drive.API)
+				.addScope(Drive.SCOPE_FILE)
+				.build();
 		initialize(new MTDGame(), config);
 	}
 }
