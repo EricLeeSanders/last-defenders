4360ce739a74bdd24831b553d529a56699ccd191
Eric Sanders <EricLeeSanders@gmail.com>
2017-03-02 22:09:38 -0500

    160 - fixed acra
    fixed acra

---
+++ android/src/com/foxholedefense/android/FHDApplicationLauncher.java
@@ -0,0 +1,39 @@
+package com.foxholedefense.android;
+
+import android.app.Application;
+
+import com.foxholedefense.config.AcraProperties;
+
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
+
+@ReportsCrashes(
+	    formUri = AcraProperties.URI,
+	    reportType = HttpSender.Type.JSON,
+	    httpMethod = HttpSender.Method.POST,
+	    formUriBasicAuthLogin = AcraProperties.LOGIN,
+	    formUriBasicAuthPassword = AcraProperties.PASSWORD,
+	    customReportContent = {
+	            ReportField.APP_VERSION_CODE,
+	            ReportField.APP_VERSION_NAME,
+	            ReportField.ANDROID_VERSION,
+	            ReportField.PACKAGE_NAME,
+	            ReportField.REPORT_ID,
+	            ReportField.BUILD,
+	            ReportField.STACK_TRACE
+	    },
+	    mode = ReportingInteractionMode.TOAST,
+		resToastText = R.string.toast_crash
+	)
+
+	public class FHDApplicationLauncher extends Application {
+	   
+	   @Override
+	   public void onCreate() {
+	        super.onCreate();
+	        ACRA.init(this);
+	   }
+	}
