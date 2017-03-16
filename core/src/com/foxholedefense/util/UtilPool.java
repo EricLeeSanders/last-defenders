92f60227b5838e43b130fe781d48d2b3140781c4
Eric Sanders <EricLeeSanders@gmail.com>
2017-03-16 14:02:28 -0400

    157 - changed enemy path actions

--- core/src/com/foxholedefense/util/UtilPool.java
+++ core/src/com/foxholedefense/util/UtilPool.java
@@ -1,15 +1,36 @@
 package com.foxholedefense.util;
 
 
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
+import com.foxholedefense.action.FHDSequenceAction;
+import com.foxholedefense.action.WaypointAction;
+import com.foxholedefense.game.model.actor.GameActor;
 
 /**
  * Created by Eric on 3/11/2017.
  */
 
 public class UtilPool {
+
+    public static FHDSequenceAction getSequenceAction(){
+        Pool<FHDSequenceAction> pool = Pools.get(FHDSequenceAction.class);
+        FHDSequenceAction sequenceAction = pool.obtain();
+        return sequenceAction;
+    }
+
+    public static WaypointAction getWaypointAction(float x, float y, float duration, float rotation, Interpolation interpolation){
+        Pool<WaypointAction> pool = Pools.get(WaypointAction.class);
+        WaypointAction waypointAction = pool.obtain();
+        waypointAction.setRotation(rotation);
+        waypointAction.setPosition(x,y);
+        waypointAction.setDuration(duration);
+        waypointAction.setInterpolation(interpolation);
+        waypointAction.setPool(pool);
+        return waypointAction;
+    }
 
     public static FHDVector2 getVector2(float x, float y){
         Pool<FHDVector2> pool = Pools.get(FHDVector2.class);
