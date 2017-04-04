b77b252a382060d3b1ce1cfd000bad5d514febca
Eric Sanders <EricLeeSanders@gmail.com>
2017-04-04 18:16:15 -0400

    225 - fixed fully qualified name

--- core/src/com/foxholedefense/game/model/actor/effects/texture/animation/death/DeathEffect.java
+++ core/src/com/foxholedefense/game/model/actor/effects/texture/animation/death/DeathEffect.java
@@ -3,12 +3,14 @@ package com.foxholedefense.game.model.actor.effects.texture.animation.death;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
+import com.foxholedefense.game.model.actor.effects.texture.TextureEffect;
+import com.foxholedefense.game.model.actor.effects.texture.animation.AnimationEffect;
 import com.foxholedefense.util.datastructures.Dimension;
 
-public abstract class DeathEffect extends com.foxholedefense.game.model.actor.effects.texture.animation.AnimationEffect {
+public abstract class DeathEffect extends AnimationEffect {
 	private static final float FRAME_DURATION = 0.05f;
 
-	public DeathEffect(Pool<com.foxholedefense.game.model.actor.effects.texture.TextureEffect> pool, Array<AtlasRegion> regions, Dimension textureSize) {
+	public DeathEffect(Pool<TextureEffect> pool, Array<AtlasRegion> regions, Dimension textureSize) {
 		super(pool, regions, textureSize, FRAME_DURATION);
 	}
 
