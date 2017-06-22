ec0f1db46ad255b49e50adde68526cf24c37376b
Eric Sanders <EricLeeSanders@gmail.com>
2017-06-22 19:36:32 -0400

    257 - changed access level

--- core/src/com/foxholedefense/util/datastructures/pool/FHDPoolable.java
+++ core/src/com/foxholedefense/util/datastructures/pool/FHDPoolable.java
@@ -8,6 +8,6 @@ import static com.badlogic.gdx.Input.Keys.T;
  * Created by Eric on 3/11/2017.
  */
 
-public interface FHDPoolable extends Pool.Poolable{
+interface FHDPoolable extends Pool.Poolable{
     void free();
 }
