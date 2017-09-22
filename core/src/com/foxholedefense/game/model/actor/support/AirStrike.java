695248f25d3e1aa2cdbecdf9be822e326e618a60
Eric Sanders <EricLeeSanders@gmail.com>
2017-09-22 00:38:20 -0400

    195 - added new graphics

--- core/src/com/foxholedefense/game/model/actor/support/AirStrike.java
+++ core/src/com/foxholedefense/game/model/actor/support/AirStrike.java
@@ -29,7 +29,7 @@ public class AirStrike extends SupportActor implements IRocket {
 
     private static final Vector2 GUN_POS = UtilPool.getVector2();
     private static final Dimension ROCKET_SIZE = new Dimension(46, 10);
-    private static final Dimension TEXTURE_SIZE = new Dimension(203, 125);
+    private static final Dimension TEXTURE_SIZE = new Dimension(206, 125);
 
     private Array<AirStrikeLocation> airStrikeLocations = new Array<>();
     private ProjectileFactory projectileFactory;
