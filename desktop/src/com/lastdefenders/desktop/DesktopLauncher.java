29f7afb4524dc76656cc724800ce253a864829ee
Eric Sanders <EricLeeSanders@gmail.com>
2018-05-29 18:04:26 -0400

    321 changed game viewport to stretch

--- desktop/src/com/lastdefenders/desktop/DesktopLauncher.java
+++ desktop/src/com/lastdefenders/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ import com.lastdefenders.LDGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 640;//640x360 480x320
-        config.height = 360;
+		config.width = 800;//640x360 480x320
+        config.height = 400;
 		config.vSyncEnabled = false;
 		config.foregroundFPS = 0;
 		new LwjglApplication(new LDGame(), config);
