81c11472a1d4ad573a3425efee262e9cf75fd3c2
Eric Sanders <EricLeeSanders@gmail.com>
2018-08-06 19:21:46 -0400

    281 - added google ads

---
+++ desktop/src/com/lastdefenders/desktop/AdControllerImpl.java
@@ -0,0 +1,15 @@
+package com.lastdefenders.desktop;
+
+import com.lastdefenders.ads.AdController;
+
+/**
+ * Created by Eric on 8/6/2018.
+ */
+
+public class AdControllerImpl implements AdController {
+
+    @Override
+    public void showInterstitialAd() {
+        // Do nothing, ads are not enabled on desktop.
+    }
+}
