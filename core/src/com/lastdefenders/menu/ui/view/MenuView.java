dad2f419b4d90a6e89ae4170233c1c1e1b72cd73
Eric Sanders <EricLeeSanders@gmail.com>
2019-01-26 01:45:01 -0500

    414 - added new assets - wip

--- core/src/com/lastdefenders/menu/ui/view/MenuView.java
+++ core/src/com/lastdefenders/menu/ui/view/MenuView.java
@@ -5,12 +5,15 @@ import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Align;
 import com.lastdefenders.menu.ui.MenuPresenter;
 import com.lastdefenders.menu.ui.view.interfaces.IMenuView;
@@ -44,14 +47,28 @@ public class MenuView extends Group implements IMenuView {
 
         Logger.info("Menu view: initializing view");
 
+        ButtonStyle menuButtonStyle = new ImageButtonStyle();
+        menuButtonStyle.up = new Image(resources.getAsset(Resources.MENU_ATLAS, TextureAtlas.class).findRegion("button_normal")).getDrawable();
+        menuButtonStyle.down = new Image(resources.getAsset(Resources.MENU_ATLAS, TextureAtlas.class).findRegion("button_press")).getDrawable();
+        menuButtonStyle.over = new Image(resources.getAsset(Resources.MENU_ATLAS, TextureAtlas.class).findRegion("button_over")).getDrawable();
+        menuButtonStyle.checked = new Image(resources.getAsset(Resources.MENU_ATLAS, TextureAtlas.class).findRegion("button_normal")).getDrawable();
+        menuButtonStyle.disabled = new Image(resources.getAsset(Resources.MENU_ATLAS, TextureAtlas.class).findRegion("button_disabled")).getDrawable();
+
         Skin skin = resources.getSkin();
-        TextButton btnPlay = new TextButton("PLAY", skin, "transparent");
-        btnPlay.setSize(126, 56);
+
+        ImageButton btnPlay = new ImageButton(skin, "resume");
+        ImageButtonStyle btnPlayStyle = new ImageButtonStyle(menuButtonStyle);
+        btnPlayStyle.imageUp = btnPlay.getStyle().imageUp;
+        btnPlay.setStyle(btnPlayStyle);
+        btnPlay.setSize(64, 64);
         btnPlay.setPosition(getStage().getViewport().getWorldWidth() / 2, getStage().getViewport().getWorldHeight() / 2, Align.center);
         addActor(btnPlay);
         setBtnPlayListener(btnPlay);
 
         btnSound = new ImageButton(skin, "sound");
+        ImageButtonStyle btnSoundStyle = new ImageButtonStyle(menuButtonStyle);
+        btnSoundStyle.imageUp = btnSound.getStyle().imageUp;
+        btnSound.setStyle(btnSoundStyle);
         btnSound.setSize(64, 64);
         btnSound.getImageCell().size(34, 32);
         btnSound.setPosition(175, 22);
@@ -59,6 +76,9 @@ public class MenuView extends Group implements IMenuView {
         setBtnSoundListener(btnSound);
 
         btnMusic = new ImageButton(skin, "music");
+        ImageButtonStyle btnMusicStyle = new ImageButtonStyle(menuButtonStyle);
+        btnMusicStyle.imageUp = btnMusic.getStyle().imageUp;
+        btnMusic.setStyle(btnMusicStyle);
         btnMusic.setSize(64, 64);
         btnMusic.getImageCell().size(28, 36);
         btnMusic.setPosition(250, 22);
@@ -66,6 +86,9 @@ public class MenuView extends Group implements IMenuView {
         setBtnMusicListener(btnMusic);
 
         ImageButton btnOptions = new ImageButton(skin, "options");
+        ImageButtonStyle btnOptionsStyle = new ImageButtonStyle(menuButtonStyle);
+        btnOptionsStyle.imageUp = btnOptions.getStyle().imageUp;
+        btnOptions.setStyle(btnOptionsStyle);
         btnOptions.setSize(64, 64);
         btnOptions.getImageCell().size(34, 35);
         btnOptions.setPosition(325, 22);
@@ -73,6 +96,9 @@ public class MenuView extends Group implements IMenuView {
         setBtnOptionsListener(btnOptions);
 
         ImageButton btnPlayServices = new ImageButton(skin, "games");
+        ImageButtonStyle btnPlayServicesStyle = new ImageButtonStyle(menuButtonStyle);
+        btnPlayServicesStyle.imageUp = btnPlayServices.getStyle().imageUp;
+        btnPlayServices.setStyle(btnPlayServicesStyle);
         btnPlayServices.setSize(64, 64);
         btnPlayServices.getImageCell().size(36, 33);
         btnPlayServices.setPosition(400, 22);
@@ -162,12 +188,12 @@ public class MenuView extends Group implements IMenuView {
     @Override
     public void setBtnSoundOn(boolean soundOn) {
 
-        btnSound.setChecked(soundOn);
+        btnSound.setDisabled(!soundOn);
     }
 
     @Override
     public void setBtnMusicOn(boolean musicOn) {
 
-        btnMusic.setChecked(musicOn);
+        btnMusic.setDisabled(!musicOn);
     }
 }
---
+++ files/batch/packing batch/menu.bat
@@ -0,0 +1,3 @@
+call java -cp gdx.jar;gdx-tools.jar com.badlogic.gdx.tools.texturepacker.TexturePacker "C:\Users\Eric\Documents\GitHub\last-defenders\files\assets\menu\lo" "C:\Users\Eric\Documents\GitHub\last-defenders\android\assets\menu\lo" menu
+call java -cp gdx.jar;gdx-tools.jar com.badlogic.gdx.tools.texturepacker.TexturePacker "C:\Users\Eric\Documents\GitHub\last-defenders\files\assets\menu\med" "C:\Users\Eric\Documents\GitHub\last-defenders\android\assets\menu\med" menu
+call java -cp gdx.jar;gdx-tools.jar com.badlogic.gdx.tools.texturepacker.TexturePacker "C:\Users\Eric\Documents\GitHub\last-defenders\files\assets\menu\hi" "C:\Users\Eric\Documents\GitHub\last-defenders\android\assets\menu\hi" menu
