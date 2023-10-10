35c68ee8b7adce952077b7f96f431e9bd30f1936
Eric Sanders <EricLeeSanders@gmail.com>
2023-10-09 22:14:08 -0400

    clear buffer

--- core/src/com/lastdefenders/screen/AbstractScreen.java
+++ core/src/com/lastdefenders/screen/AbstractScreen.java
@@ -36,7 +36,8 @@ public abstract class AbstractScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(0,0,0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         renderElements(delta);
         //profile();
 
